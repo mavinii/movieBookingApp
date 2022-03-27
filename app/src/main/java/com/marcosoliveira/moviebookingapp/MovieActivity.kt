6a21a2154fdf2c1078d84727b946cf2c2dfc6ef1
mavinii <pg.marcosoliveira@icloud.com>
2022-03-27 02:13:25 +0100

    Generate a random number still missing;

--- app/src/main/java/com/marcosoliveira/moviebookingapp/MovieActivity.kt
+++ app/src/main/java/com/marcosoliveira/moviebookingapp/MovieActivity.kt
@@ -1,5 +1,6 @@
 package com.marcosoliveira.moviebookingapp
 
+import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import org.w3c.dom.Text
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_movie.*
 
 class MovieActivity : AppCompatActivity() {
 
@@ -36,6 +38,11 @@ class MovieActivity : AppCompatActivity() {
         descriptionSecondScreen.text = description
         starringSecondScreen.text = starring
         runningTimeSecondScreen.text = runningTime
+
+        btn_book_now.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     //    ERROR FOUND, I WAS TRYING TO EXECUTE THIS CODE OUSIDE OF MY METHOD ONCREATE
